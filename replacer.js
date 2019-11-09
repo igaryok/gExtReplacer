6eab89c5838e73c0c2ac6dbecdeba9e863ebef9e
igaryok <igor.sobko@gmail.com>
2019-11-09 22:11:07 +0200

    refactored project into one script

--- replacer.js
+++ replacer.js
@@ -1,18 +1,25 @@
-const DIC_REPLACER_CON = {
+const DIC_REPLACER = {
   'foo': 'bar',
   'cat': 'dog',
   'helo': 'hello',
   'heldp': 'help',
 }
-const KEY_REPLACER_CON= Object.keys(DIC_REPLACER_CON);
+const KEY_REPLACER = Object.keys(DIC_REPLACER);
+
+const replacerInput = ({ target }) => {
+
+  KEY_REPLACER.forEach(item => (
+    target.value = target.value.replace(item, DIC_REPLACER[item]
+  )));
+}
 
 const replacerContenteditable = ({ target }) => {
   
-  KEY_REPLACER_CON.forEach(item => {
+  KEY_REPLACER.forEach(item => {
     const range = document.createRange();
     const selection = window.getSelection();
 
-    target.innerHTML = target.innerHTML.replace(item, DIC_REPLACER_CON[item]);
+    target.innerHTML = target.innerHTML.replace(item, DIC_REPLACER[item]);
     
     range.selectNodeContents(target);
     range.collapse(false);
@@ -22,16 +29,24 @@ const replacerContenteditable = ({ target }) => {
   });
 }
 
-const enableReplacerContenteditable = () => {
+const enableReplacer = () => {
+  const inputs = document.querySelectorAll('input[type="text"]');
   const contenteditables = document.querySelectorAll("*[contenteditable='true']");
   
+  if(inputs.length) {
+    inputs.forEach(item => (
+      item.addEventListener("keypress", event => replacerInput(event)
+    )));
+    console.log("download replacer for inputs");
+  }
+
   if(contenteditables.length) {
     contenteditables.forEach(item => (
       item.addEventListener("keypress", event => replacerContenteditable(event)
     )));
     console.log("download replacer for contenteditables");
   }
-  
+
 }
 
-enableReplacerContenteditable();
+enableReplacer();
